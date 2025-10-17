13d401512e10d483a88b927346b8942cc04eb9dd
bogdog2b <120654221@qq.com>
2025-10-17 12:48:59 -0400

    Update Member4.java

--- Group_Lab_1/src/Member4.java
+++ Group_Lab_1/src/Member4.java
@@ -7,7 +7,7 @@
  *
  * @author Administrator
  */
-public class Member {
+public class Member4 {
     public static void main(String[] args) {
         System.out.println("Hello from Member 4!");
     }    
